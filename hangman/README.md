ed3d8175efa450ed31e3e2646549c1815e9b0497
nabitanvir <93293961+nabitanvir@users.noreply.github.com>
2024-02-22 16:54:15 -0600

    moved project for organization

---
+++ hangman/README.md
@@ -0,0 +1,12 @@
+# hangman
+Hangman game that ties JavaScript and HTML
+
+Project meeting 10/30/21
+
+Code methods:
+1) Welcome (rules)
+2) Computer side (generating array)
+3) Asking user for inputs / handling
+4) step 2 + step 3 + method for hangman
+
+Collaborators: @prasoontandon
---
+++ hangman/hangmanProjectMain.java
@@ -0,0 +1,17 @@
+
+
+public class hangmanProjectMain {
+
+	public static void main(String[] args) {
+	
+		// word is assigned to variable
+		String hangmanWord = hangmanProjectWord.wordGenerator();
+		
+		// test
+		System.out.print("The code somehow properly pulled the word: " + hangmanWord);
+
+		
+		
+	}
+
+}
