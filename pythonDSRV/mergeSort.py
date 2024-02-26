6070264a4d2faa000df9524879e646c1eb83b94e
nabitanvir <93293961+nabitanvir@users.noreply.github.com>
2024-02-25 22:57:17 -0600

    completed mergesort

--- pythonDSRV/mergeSort.py
+++ pythonDSRV/mergeSort.py
@@ -18,19 +18,33 @@ def mergeSort(array):
         #initialize temp values
         i, j, k = 0, 0, 0
 
+        # 
+        while i < len(leftHalf) and j < len(rightHalf):
+            if leftHalf[i] <= rightHalf[j]:
+                array[k] = leftHalf[i]
+                i += 1
+            else:
+                array[k] = rightHalf[j]
+                j += 1
+            k += 1
+
+        # check for remaining elements
+        while i < len(leftHalf):
+            array[k] = leftHalf[i]
+            i += 1
+            k += 1
         
-
-
-        
-
-
-        
-
+        while j < len(rightHalf):
+            array[k] = rightHalf[j]
+            j += 1
+            k += 1
 
 def main():
     # note: duplicate nums in list
     nums = [60, 20, 5, 50, 30, 70, 43, 59, 2, 1, 52, 75, 52]
     print("list before mergeSort: " + str(nums))
+    mergeSort(nums)
+    print("list after sort is: " + str(nums))
 
 if __name__ == "__main__":
     main()
