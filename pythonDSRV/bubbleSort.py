c7799392ff02f825a3eeff2841588dd592f682d2
nabitanvir <93293961+nabitanvir@users.noreply.github.com>
2024-02-26 14:19:15 -0600

    completed bubble sort

--- pythonDSRV/bubbleSort.py
+++ pythonDSRV/bubbleSort.py
@@ -1,10 +1,25 @@
 #my attempt at a bubble sort
 
 def bubbleSort(arr):
-    return 0
+    length = len(arr)
+    
+    # check every element in the array
+    for i in range(length):
+        swapped = False
+
+        for j in range(0, length-i-1):
+            # if prev element is greater than next, switch
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if (swapped == False):
+            break
 
 def main():
-    return 0
+    prices = [36.99, 12.42, 42.00, 53.22, 145, 98.77, 24.4]
+    print("unsorted: " + str(prices))
+    bubbleSort(prices)
+    print("sorted: " + str(prices))
 
 if __name__ == "__main__":
     main()
